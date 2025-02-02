95bd32f7772209da175dbd5c5d2706478f6df57c
MayraOishi <namie.oishi@gmail.com>
2025-02-02 16:27:06 -0300

    Desafio pronto

--- atividade.py
+++ atividade.py
@@ -5,9 +5,15 @@ if xp <= 1000:
     nivel = "Ferro"
 elif 1001 <= xp <= 2000:
     nivel = "Bronze"
-elif xp >= 2001:
+elif 2001 <= xp <= 5000:
     nivel = "Prata"
+elif 5001 <= xp <= 8000:
+    nivel = "Platina Diamante"
+elif 8001 <= xp <= 9000:
+    nivel = "Ascendente"
+elif 9001 <= xp <= 10000:
+    nivel = "Imortal"
 else:
-    nivel = "Desconhecido"
+    nivel = "Radiante"
 
 print(f"O herói de nome {nome} está no nível de {nivel}.")
